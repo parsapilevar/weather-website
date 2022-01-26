7afdeb14eab55c549005ad5a20ad9f241b8cd916
unknown <parsapil01@gmail.com>
2022-01-26 05:36:06 +0330

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,7 @@
-console.log('Client side java script file is loaded!')
-
-// fetch('https://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const massageOne = document.querySelector('#massage-1')
 const massageTwo = document.querySelector('#massage-2')
-
-// massageOne.textContent = 'dd'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
